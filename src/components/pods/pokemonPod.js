78e022c5730d0c8ceae771ab6be6f9e2afb3e557
Aaron Fryer <aaron@iMac-2.local>
2017-03-27 17:40:59 +0100

    small update to pokepod

--- src/components/pods/pokemonPod.js
+++ src/components/pods/pokemonPod.js
@@ -9,9 +9,12 @@ class Body extends Component {
 
   render() {
     return (
-      <div className={ "pokePod half bg-red pad-20" }>
-        <div className="bg-white pod-img">
+      <div className={ "pokepod half bg-red pad-20 hidden" }>
+        <div className="bg-white pod-img left">
           <img src={ this.props.pokemon.sprites.front_default } />
+        </div>
+        <div className="margin-left-20 white left">
+          <p>{ this.props.pokemon.name }</p>
         </div>
       </div>
     );
